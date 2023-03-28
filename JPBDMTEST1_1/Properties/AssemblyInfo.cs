ca44dd434e3596516502eb6ab5197ba41b6d70d2
jpb <jpb@BGOLAP-JPB>
2023-03-28 14:07:53 +0200

    Add project files.

---
+++ JPBDMTEST1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JPBDMTEST1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("JPBDMTEST1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A876DAD3-C292-4966-A553-FC1FB575F12B")]
+[assembly: AssemblyVersion("1.0.0.0")]
